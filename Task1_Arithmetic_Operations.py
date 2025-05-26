b79bbda585f6fc316c6cea78635137165e2b3daa
amitkrathore0411 <amitrathore0411@gmail.com>
2025-05-26 15:55:44 +0530

    Add files via upload

---
+++ Task1_Arithmetic_Operations.py
@@ -0,0 +1,15 @@
+'''Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division'''
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second Number : "))
+
+print("Addition: ",n1+n2)
+print("Subtraction: ",n1-n2)
+print("Multiplication: ",n1*n2)
+print("Division: ",n1/n2)
+
---
+++ Task2_Create a Personalized Greeting.py
@@ -0,0 +1,9 @@
+'''Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+first = input("Enter your First Name : ")
+last = input("Enter your last Name : ")
+print("Hello "+first+" "+last+"! Welcome to Python Program")
