a5e581e31e3ec02d6a66332bd65be04f88731f4f
amitkrathore0411 <amitrathore0411@gmail.com>
2025-05-29 12:37:00 +0530

    Update Assignement3_Task1_Calculate Factorial Using a Function.py

--- Assignement3_Task1_Calculate Factorial Using a Function.py
+++ Assignement3_Task1_Calculate Factorial Using a Function.py
@@ -1,8 +1,11 @@
 def factorial(n):
-    fact=1;
-    for i in range(n,1,-1):
-        fact*=i;
-    return fact
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
 
 num=int(input("Enter number whose fact is to be printed : "))
-print(factorial(num))
+if num<0:
+    print("Factorial can not be calculated for negative no")
+else:
+    print("Factorial of given number = ",factorial(num))
